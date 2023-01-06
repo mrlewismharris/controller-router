b7486b996085e59e2c52d03696e3c5b71c0ed747
lewish7895 <lewis.harris@sdc.co.uk>
2023-01-06 20:37:22 +0000

    changed concat'ed string to path.join() for compatibility

--- Controllers/Index/IndexController.js
+++ Controllers/Index/IndexController.js
@@ -1,8 +1,9 @@
 var eta = require('eta')
+const path = require('path')
 
 module.exports = {
     Index: async (data) => {
-        return await eta.renderFile(__dirname + "/Views/Index.eta", {
+        return await eta.renderFile(path.join(__dirname, '/Views/Index.eta'), {
             root: data.root,
             developer: data.serverData.environment == "dev"
         })
