6052f80e3fa0e31d6aa1a0cc0e07eee0122d6954
lewish7895 <lewis.harris@sdc.co.uk>
2023-01-07 13:50:54 +0000

    fixed path for cross OS compatibility

--- Controllers/Index/IndexController.js
+++ Controllers/Index/IndexController.js
@@ -3,7 +3,7 @@ const path = require('path')
 
 module.exports = {
     Index: async (data) => {
-        return await eta.renderFile(path.join(__dirname, '/Views/Index.eta'), {
+        return await eta.renderFile(path.join(__dirname, 'Views/Index.eta'), {
             root: data.root,
             developer: data.serverData.environment == "dev"
         })
