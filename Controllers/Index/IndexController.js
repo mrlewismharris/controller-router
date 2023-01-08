bffe3ef71988d0bc70f0a4e09a2f688d575e0d05
lewish7895 <lewis.harris@sdc.co.uk>
2023-01-08 00:41:03 +0000

    another attempt to fix cross platform path issue

--- Controllers/Index/IndexController.js
+++ Controllers/Index/IndexController.js
@@ -1,9 +1,11 @@
+var path = require('path')
+
 module.exports = {
     Index: async (data) => {
         return await data.eta.renderFile('Views/Index.eta', {
             root: data.root,
             developer: data.serverData.environment == "dev",
-            views: __dirname
+            views: path.normalize(__dirname)
         })
     }
 }
--- Controllers/Test/TestController.js
+++ Controllers/Test/TestController.js
@@ -1,9 +1,11 @@
+var path = require('path')
+
 module.exports = {
     Test: async (data) => {
         return await data.eta.renderFile('Views/Test.eta', {
             root: data.root,
             developer: data.serverData.environment == "dev",
-            views: __dirname
+            views: path.normalize(__dirname)
         })
     },
     ApiCall: () => {
